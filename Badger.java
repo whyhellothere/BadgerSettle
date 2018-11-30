4f56b9e03c4cdfefdfdceb27ed2ddc7c6ff66b78
Daniel <dchu22@wisc.edu>
2018-11-29 19:10:05 -0600

    fixes

--- Badger.java
+++ Badger.java
@@ -1,3 +1,37 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Access Control
+// Files: User.Java, AccessControlTest.java, AccessControl.java
+// Course: CS300, Fall, 2018
+//
+// Author: Daniel Chu
+// Email: dchu22@wisc.edu
+// Lecturer's Name: Gary Dahl
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name: Tolga Beser
+// Partner Email: tbeser@wisc.edu
+// Partner Lecturer's Name: Gary Dahl
+//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+// _x_ Write-up states that pair programming is allowed for this assignment.
+// _x_ We have both read and understand the course Pair Programming Policy.
+// _x_ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: N/A
+// Online Sources: N/A
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
 /**
  * This class represents a Badger which is designed to live in a Sett. Each Badger object represents a single node within a BST (known as a Sett).
  *
@@ -57,7 +91,5 @@ public class Badger {
   public void setRightLowerNeighbor(Badger badger) {
     rightLowerNeighbor = badger;
   }
-  
-  
   
 }
--- P9Tests.java
+++ P9Tests.java
@@ -1,3 +1,37 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Access Control
+// Files: User.Java, AccessControlTest.java, AccessControl.java
+// Course: CS300, Fall, 2018
+//
+// Author: Daniel Chu
+// Email: dchu22@wisc.edu
+// Lecturer's Name: Gary Dahl
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name: Tolga Beser
+// Partner Email: tbeser@wisc.edu
+// Partner Lecturer's Name: Gary Dahl
+//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+// _x_ Write-up states that pair programming is allowed for this assignment.
+// _x_ We have both read and understand the course Pair Programming Policy.
+// _x_ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: N/A
+// Online Sources: N/A
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
 import java.util.ArrayList;
 
 /**
@@ -36,7 +70,7 @@ public class P9Tests {
 
   /**
    * Adds several badgers to left nodes, and checks if values are correct
-   * @return
+   * @return boolean describing if tests passed
    */
   private static boolean testBadgerLeft() {
     boolean passed = true; // boolean that is returned
@@ -60,6 +94,10 @@ public class P9Tests {
     return passed;
   }
 
+  /**
+   * Adds several badgers to right nodes and checks if values are correct
+   * @return boolean describing if tests passed
+   */
   private static boolean testBadgerRight() {
     boolean passed = true; // boolean that is returned
 
@@ -158,7 +196,5 @@ public class P9Tests {
     return passed;
 
   }
-
-
 
 }
